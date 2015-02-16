cb6dc3fb0dcaf72e804d7dff5db47a447b10225b
anfego <anfego22@gmail.com>
2015-02-16 18:26:26 -0500

    Set FXtoBSON function, not working

--- tutorial2.cpp
+++ tutorial2.cpp
@@ -8,14 +8,8 @@
 
 using namespace std;
 using namespace mongo;
-
-int main(int argc, char *argv[])
-{
-  mongo::client::initialize();
-  DBClientConnection cc;
-  cc.connect("localhost");
-  
-  std::string file(argv[1]);
+/*
+void FXtoBSON(const string file){
   std::ifstream csvFile;
   std::string dropheader, line, cell;
   csvFile.open(file);
@@ -50,6 +44,19 @@ int main(int argc, char *argv[])
     BSONObj doc = dc.obj();
     cout << doc.toString() << endl;
   }
+}
+*/
+
+int main(int argc, char *argv[])
+{
+  mongo::client::initialize();
+  DBClientConnection c;
+  c.connect("localhost");
+  std::string file(argv[1]);
+  
+  //FXtoBSON(file);
+  cout << argc << endl;
+  mongo::client::shutdown();
   return EXIT_SUCCESS;
 }
 
