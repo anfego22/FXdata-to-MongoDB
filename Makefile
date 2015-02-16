2201ccc4a39b543a55e76f7c8595d4da8b64e50c
anfego <anfego22@gmail.com>
2015-02-16 15:03:53 -0500

    Create BSON Objects throw a csv file

--- Makefile
+++ Makefile
@@ -2,14 +2,14 @@ CC=g++
 LIBS=-L/usr/include/mongo/lib -L/usr/share/boost-buil/ -pthread -lmongoclient -lboost_thread -lboost_system -lboost_regex
 IDIR =-I/usr/include/boost_1_57_0 -I/usr/include/mongo/include
 CFLAGS=-c -w
-OBJ = tutorial.o
+OBJ = tutorial2.o
 all: TT
 
 TT: $(OBJ) 
 	$(CC) $^ -o $@ $(LIBS) $(IDIR) 
 
-%.o: %.cpp 
-	$(CC) $(CFLAGS) $(IDIR) $(LIBS) $^ 
+$(OBJ): tutorial2.cpp 
+	$(CC) $(CFLAGS) $(IDIR) $(LIBS) $^ -std=c++11 
 
 clean:
 	rm -rf *o TT
--- tutorial2.cpp
+++ tutorial2.cpp
@@ -4,33 +4,52 @@
 #include <sstream>
 #include <fstream>
 #include <string>
-
+#include <vector>
 
 using namespace std;
 using namespace mongo;
-namespace fs = boost::filesystem; 
 
 int main(int argc, char *argv[])
 {
-    client::initialize();
-  DBClientConnection c;
-  c.connect("localhost");
+  mongo::client::initialize();
+  DBClientConnection cc;
+  cc.connect("localhost");
   
-  string file(argv[1]);
-  ifstream csvFile;
-  string dropheader, line;
+  std::string file(argv[1]);
+  std::ifstream csvFile;
+  std::string dropheader, line, cell;
   csvFile.open(file);
   getline(csvFile, dropheader);
-  while(getline(csvFile, line, ';')){
-    /*
-      BSONObj doc = BSON(GENOID, "Date" << line << "Open" << line
-      << "High" << line << "Low" << line << "Close"
-      << line << "Vol" << line);
-      cout << doc.toString() << endl;
-    */
-    cout << line << endl;
+  while(getline(csvFile, line)){
+    std::string tmTime;
+    std::stringstream lineS(line);
+    vector<double> quotes;
+    quotes.reserve(5);
+    for (int i = 0; i<=5; i++){
+      getline(lineS, cell, ';');
+      std::istringstream ss(cell);
+      switch(i){
+      case 0:
+	{
+	  ss >> tmTime;
+	}
+	break;
+      default:
+	{
+	  double d;
+	  ss >> d;
+	  quotes.push_back(d);
+	}
+	break;
+      }
+    }
+    BSONObjBuilder dc;
+    dc.append("Date", tmTime).append("Open", quotes[0]);
+    dc.append("High", quotes[1]).append("Low", quotes[2]);
+    dc.append("Close", quotes[3]).append("Vol", quotes[4]);
+    BSONObj doc = dc.obj();
+    cout << doc.toString() << endl;
   }
-  
   return EXIT_SUCCESS;
 }
 
