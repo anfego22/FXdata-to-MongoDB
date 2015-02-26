8d8d0bb5a51317d07423ce31323c9787b9cdef20
anfego <anfego22@gmail.com>
2015-02-26 17:19:03 -0500

    New schema for storing data, in development

--- BSONfx.cpp
+++ BSONfx.cpp
@@ -20,19 +20,21 @@ void FXtoBSON::headers(){
 
 FXtoBSON::FXtoBSON(const string &file_, const string &formatt_,
 		   const string &pair, DBClientConnection &c, const char &sep_):
-  file(file_), cols(0), formatt(formatt_), h(0), sep(sep_){
+  file(file_), cols(0), formatt(formatt_), h(-1), sep(sep_){
   db = "FOREX.";
   db += pair;
   string dropheader, line, cell;
   int j = 0;
   struct tm tempTM;
   headers();
+  //Years();
   csvFile.seekg(0);
   csvFile.open(file.c_str());
   getline(csvFile, dropheader);
+  BSONObjBuilder Open, High, Low, Close, Vol;
   while(getline(csvFile, line)){
     string query;
-    BSONObjBuilder quotes, FIND;
+    BSONObjBuilder FIND;
     istringstream lineS(line);
     for (int i = 0; i!=cols; i++){
       getline(lineS, cell, sep);
@@ -47,18 +49,29 @@ FXtoBSON::FXtoBSON(const string &file_, const string &formatt_,
     }
     string min = to_string(tempTM.tm_min);
     string hour = to_string(tempTM.tm_hour);
-    string day = to_string(tempTM.tm_mday);
-    string mon = to_string(tempTM.tm_mon);
-    string p = ".";
-    query = min;
-    tempTM.tm_min = 0;
-    FIND.appendTimeT("Date", mktime(&tempTM)); 
-    try{
-      c.update(db, FIND.obj(),
-	       BSON("$addToSet" << BSON(query << quotes.obj())), true);
-    } catch( const mongo::DBException &e) {
-      std::cout << "caught " << e.what() << std::endl;
-    }
+      if(h != tempTM.tm_hour | j == T-1){
+	tempTM.tm_min = 0;
+	FIND.appendTimeT("Date", mktime(&tempTM)); 
+	try{
+	  c.update(db, FIND.obj(),
+		   BSON("$addToSet" << BSON("Open" << Open.obj() <<
+					    "High" << High.obj() <<
+					    "Low" << Low.obj() <<
+					    "Close" << Close.obj() <<
+					    "Vol" << Vol.obj())), true);
+	} catch( const mongo::DBException &e) {
+	  std::cout << "caught " << e.what() << std::endl;
+	}
+      } else {
+	BSONObj QUOTE = quotes.obj();
+	Open.append(min, QUOTE.getField("Open").numberDouble());
+	High.append(min, QUOTE.getField("High").numberDouble());
+	Low.append(min, QUOTE.getField("Low").numberDouble());
+	Close.append(min, QUOTE.getField("Close").numberDouble());
+	Vol.append(min, QUOTE.getField("Vol").numberDouble());
+      }
+      h = tempTM.tm_hour;
+      j++;
   } // While end;
 }
 
--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC=g++
 LIBS=-L/usr/include/mongo/lib -L/usr/share/boost-buil/ -pthread -lmongoclient -lboost_thread -lboost_system -lboost_regex
 IDIR =-I/usr/include/boost_1_57_0 -I/usr/include/mongo/include
-CFLAGS= -w
+CFLAGS= -w -ggdb
 OBJ = tutorial2.o BSONfx.o
 DEP = FXtoBSON.h
 all: TT
